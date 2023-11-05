8bde7881733bc2f4f3198b62255941859f74111e
JEAN\szuje <szujean45@gmail.com>
2023-11-06 02:09:46 +0800

    Init data used in blog, scammer and simulation

---
+++ client/constants/scammerData.ts
@@ -0,0 +1,44 @@
+export const scammers = [
+    {
+      id: 1,
+      title: 'Macao Scam',
+      image: require('../assets/images/Sim1.png'),
+      number: '+60123455633',
+      recent: '40 Recent Report'
+    },
+    {
+      id: 2,
+      title: 'Shopee Job',
+      image: require('../assets/images/Sim2.png'),
+      number: '+60123455633',
+      recent: '40 Recent Report'
+    },
+    {
+      id: 3,
+      title: 'JOB SCAM',
+      image: require('../assets/images/Sim3.png'),
+      number: '+60123455633',
+      recent: '40 Recent Report'
+    },
+    {
+      id: 4,
+      title: 'The Online Sweetheart',
+      image: require('../assets/images/Sim4.png'),
+      number: '+60123455633',
+      recent: '40 Recent Report'
+    },
+    {
+      id: 5,
+      title: 'The Banking Trick',
+      image: require('../assets/images/Sim5.png'),
+      number: '+60123455633',
+      recent: '40 Recent Report'
+    },
+    {
+      id: 6,
+      title: 'The Mystery Download',
+      image: require('../assets/images/Sim6.png'),
+      number: '+60123455633',
+      recent: '40 Recent Report'
+    }, 
+  ];
---
+++ client/constants/simulationData.ts
@@ -0,0 +1,32 @@
+export const simulations = [
+    {
+      id: 1,
+      title: 'The Starbucks Trap',
+      image: require('../assets/images/Sim1.png'),
+    },
+    {
+      id: 2,
+      title: 'The Suspicious Attachment',
+      image: require('../assets/images/Sim2.png'),
+    },
+    {
+      id: 3,
+      title: 'The Friendly Stranger',
+      image: require('../assets/images/Sim3.png'),
+    },
+    {
+      id: 4,
+      title: 'The Online Sweetheart',
+      image: require('../assets/images/Sim4.png'),
+    },
+    {
+      id: 5,
+      title: 'The Banking Trick',
+      image: require('../assets/images/Sim5.png'),
+    },
+    {
+      id: 6,
+      title: 'The Mystery Download',
+      image: require('../assets/images/Sim6.png'),
+    }, 
+  ];
