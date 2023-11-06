ca3e49510c9859c305db00fcc0ab64eb3abfef21
eugene <eugenetin99@gmail.com>
2023-11-06 22:22:11 +0800

    user controller

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,2 +1,180 @@
 import User from '../models/user';
 import ErrorHandler from '../utils/errorHandler';
+import catchAsyncErrors from './catchAsyncErrors';
+import sendToken from '../utils/jwtToken';
+
+/*
+ * Create user
+ * Path: server/controller/user.js
+ * Parameter: req, res, next
+ * Return: res
+ */
+export const createUser = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const { username, email, password, avatar } = req.body;
+    let user = await User.findOne({ username });
+    if (user) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Email already registered' });
+    }
+
+    user = await User.create({
+      username,
+      email,
+      password,
+      avatar,
+    });
+
+    sendToken(user, 201, res);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+/*
+ * Login user
+ * Path: server/controller/user.js
+ * Parameter: req, res, next
+ * Return: res
+ */
+export const loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return next(new ErrorHandler('Please enter email & password', 400));
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorHandler('Invalid email or password', 401));
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler('Invalid email or password', 401));
+  }
+
+  sendToken(user, 201, res);
+});
+
+/*
+ * Logout user
+ * Path: server/controller/user.js
+ * Parameter: req, res, next
+ * Return: res
+ */
+export const logoutUser = catchAsyncErrors(async (req, res, next) => {
+  res.cookie('token', null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+    sameSite: 'none',
+    secure: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: 'Logged out',
+  });
+});
+
+/*
+ * Get all users
+ * Path: server/controller/user.js
+ * Parameter: req, res, next
+ * Return: res
+ */
+export const getUsers = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      success: true,
+      users,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+/*
+ * Get a user
+ * Path: server/controller/user.js
+ * Parameter: req, res, next
+ * Return: res
+ */
+export const getUser = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return next(new ErrorHandler('User not found', 404));
+    }
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+/*
+ * Update a user
+ * Path: server/controller/user.js
+ * Parameter: req, res, next
+ * Return: res
+ */
+export const updateUser = catchAsyncErrors(async (req, res, next) => {
+  try {
+    let user = await User.findById(req.params.id);
+    if (!user) {
+      return next(new ErrorHandler('User not found', 404));
+    }
+
+    user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+/*
+ * Get users from a group
+ * Path: server/controller/user.js
+ * Parameter: req, res, next
+ * Return: res
+ */
+export const getUsersFromGroup = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const users = await User.find({ groups: req.params.id });
+    res.status(200).json({
+      success: true,
+      users,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
