fc420a8d5db27450af965a38c4e1dd4ffa36f734
JEAN\szuje <szujean45@gmail.com>
2023-11-06 22:23:45 +0800

    Fix background colour not covering certain parts

--- client/app/(pages)/home.tsx
+++ client/app/(pages)/home.tsx
@@ -26,8 +26,6 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     backgroundColor: color.lavender,
-    width: "100%",
-    height: "100%",
   },
   headerText: {
     fontSize: 20,
