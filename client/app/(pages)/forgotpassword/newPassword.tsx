00e073cd3c2b9f9013c7ff9b1a750ed52e05e07f
futzyunz <yxuan99@outlook.com>
2023-11-07 16:01:40 +0800

    done ui but not the logic, anyone can help?

---
+++ client/app/(pages)/forgotpassword/newPassword.tsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+} from "react-native";
+import { color } from "../../../constants/Colors";
+
+const NewPasswordScreen = () => {
+    return (
+        <View style={styles.base}>
+            <Text style={styles.title}>Forgot Password</Text>
+
+            <View style={styles.labelInputContainer}>
+                <Text style={styles.label}>New Password</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter your new password"
+                />
+            </View>
+
+            <View style={styles.labelInputContainer}>
+                <Text style={styles.label}>Confirm new password</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Confirm your new password"
+                />
+            </View>
+
+            <View style={styles.instructionsContainer}>
+                <View style={styles.rectangle}>
+                    <Text style={styles.instructionsText}>
+                        Instruction:
+                        {"\n"}
+                        Please enter a new password for your account.
+                    </Text>
+                </View>
+            </View>
+
+            <TouchableOpacity style={styles.resetPasswordButton}>
+                <Text style={styles.resetPasswordButtonText}>
+                    Reset Password
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+export default NewPasswordScreen;
+
+const styles = StyleSheet.create({
+    base: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: color.lavender,
+        padding: 16,
+    },
+    title: {
+        fontFamily: "NotoSans",
+        color: color.americanBlue,
+        fontSize: 30,
+        fontWeight: "700",
+        marginBottom: 52,
+    },
+    labelInputContainer: {
+        flexDirection: "column",
+        width: "100%",
+        height: "10%",
+        alignItems: "flex-start",
+        marginBottom: 317,
+    },
+    label: {
+        fontSize: 16,
+        marginBottom: 8,
+    },
+    input: {
+        flex: 1,
+        width: "100%",
+        color: "#453C67",
+        fontFamily: "Roboto",
+        fontSize: 14,
+        fontStyle: "normal",
+        fontWeight: "500",
+        lineHeight: 20,
+        borderWidth: 1,
+        borderColor: "rgba(0, 0, 0, 0.10)",
+        borderRadius: 6,
+        paddingHorizontal: 10,
+        backgroundColor: "#FFF",
+    },
+    instructionsContainer: {
+        alignItems: "center",
+        marginBottom: 48,
+    },
+    rectangle: {
+        width: 336,
+        height: 90,
+        backgroundColor: color.purple,
+        padding: 16,
+        borderRadius: 8,
+    },
+    instructionsText: {
+        fontSize: 12,
+        color: color.white,
+        fontFamily: "NotoSans",
+        letterSpacing: 0.144,
+    },
+    resetPasswordButton: {
+        backgroundColor: color.americanBlue,
+        width: 312,
+        paddingVertical: 12,
+        paddingHorizontal: 24,
+        borderRadius: 8,
+        shadowColor: "rgba(0, 0, 0, 0.25)",
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 1,
+        shadowRadius: 4,
+        elevation: 4,
+    },
+    resetPasswordButtonText: {
+        color: color.white,
+        fontFamily: "Roboto",
+        fontSize: 16,
+        fontWeight: "500",
+        textAlign: "center",
+    },
+});
