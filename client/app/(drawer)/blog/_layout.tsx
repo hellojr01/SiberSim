fb2adf681b142a2822aa03231dada72c45f63dda
JEAN\szuje <szujean45@gmail.com>
2023-11-09 20:11:50 +0800

    Add stack nav to pages under blog

--- client/app/(drawer)/blog/_layout.tsx
+++ client/app/(drawer)/blog/_layout.tsx
@@ -6,7 +6,29 @@ export default function BlogLayout() {
             screenOptions={{
                 title: "Cyber Blog",
                 headerShown: false,
+            }}>
+            <Stack.Screen
+            name="index"
+            options={{
+                title: "Home",
             }}
-        />
+            />
+            <Stack.Screen
+                name="addBlog"
+                options={{
+                    title: "Add Blog",
+                    presentation: "modal",
+                    headerShown: false,
+                }}
+            />
+            <Stack.Screen
+                name="[id]"
+                options={{
+                    title: "Back to Cyberblog",
+                    presentation: "modal",
+                    headerShown: true,
+                }}
+            />
+        </Stack>
     );
 }
