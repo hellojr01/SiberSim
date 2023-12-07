ce29f9c0b0e70e566eb904474d17cc41e21ff861
JEAN\szuje <szujean45@gmail.com>
2023-12-08 06:41:17 +0800

    include quiz in learning (no design yet)

--- client/app/(drawer)/learn/[id].tsx
+++ client/app/(drawer)/learn/[id].tsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity  } from 'react-native';
 import Swiper from 'react-native-swiper';
+import Modal from 'react-native-modal';
 import { Redirect, useLocalSearchParams } from 'expo-router';
 import { simulations } from '@constants/simulationData';
-import Quiz from '@components/Quiz';
+import { router } from "expo-router";
 import { color } from "@constants/Colors";
 
-const Learn = () => {
+type Props = {
+  redirect?: boolean;
+};
+
+const Learn = (redirect : Props) => {
   const { id } = useLocalSearchParams();
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
 
   let value: number | undefined = Number(id);
 
@@ -23,22 +30,72 @@ const Learn = () => {
   };
 
   const handleAnswerSelected = (selectedAnswerIndex: number) => {
-    // Handle the answer selection logic here
-    console.log(`Selected answer index: ${selectedAnswerIndex}`);
+    const correctAnswerIndex = learningDetails?.correctAnswer || 0;
+    const isCorrect = selectedAnswerIndex === correctAnswerIndex;
+
+    setIsAnswerCorrect(isCorrect);
+    setModalVisible(true);
   };
 
-  const quizData = learningDetails?.content[currentIndex];
+  let swiperContent: (JSX.Element | string)[] = [];
+
+  if (learningDetails) {
+    swiperContent = learningDetails.content.map((image, index) => (
+      <View key={index} style={styles.slide}>
+        <Image source={image} style={styles.image} />
+      </View>
+    ));
+
+    swiperContent.push(
+      <View key="additionalContent" style={styles.slide}>
+        <Text style={styles.question}>{learningDetails.question}</Text>
+        {learningDetails.choices.map((choice, index) => (
+          <TouchableOpacity key={index} onPress={() => handleAnswerSelected(index)}
+          style={styles.choiceButton}>
+            <Text  style={styles.choiceButtonText}>{choice}</Text>
+          </TouchableOpacity>
+        ))}
+        
+        <Modal isVisible={isModalVisible}>
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalTitle}>Knowledge Check Result</Text>
+            {isAnswerCorrect ? (
+              <Text style={[styles.statusText, styles.correctStatus]}>Status: Completed</Text>
+            ) : (
+              <Text style={[styles.statusText, styles.incorrectStatus]}>Status: Failed</Text>
+            )}
+            <Text style={styles.explanationText}>
+              {isAnswerCorrect
+                ? "Congratulations! You have successfully completed the knowledge check."
+                : "Unfortunately, your answer is incorrect. Here's an explanation of why:"}
+            </Text>
+            <Text style={styles.explanationDetails}>
+              {/* Add your long explanation here */}
+            </Text>
+            <TouchableOpacity
+              onPress={() => {
+                setModalVisible(false);
+                router.push("/learn");
+                redirect
+                  ? setTimeout(() => router.replace("/learn"), 5)
+                  : setTimeout(() => router.push("/learn"), 5);
+              }}
+              style={styles.returnButton}
+            >
+              <Text style={styles.returnButtonText}>Return to Main Page</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      </View>
+    );
+  } else {
+    swiperContent = ["Unknown: Report Not Available!"];
+  }
 
   return (
     <View style={styles.container}>
-      <Swiper loop={false} onIndexChanged={handleIndexChanged} showsButtons={true} showsPagination={true}>
-        {learningDetails
-          ? learningDetails.content.map((image, index) => (
-              <View key={index} style={styles.slide}>
-                <Image source={image} style={styles.image} />
-              </View>
-            ))
-          : "Unknown: Report Not Available!"}
+      <Swiper loop={false} onIndexChanged={handleIndexChanged} showsButtons={true}>
+        {swiperContent}
       </Swiper>
     </View>
   );
@@ -47,7 +104,7 @@ const Learn = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: color.lavender
+    backgroundColor: color.lavender,
   },
   slide: {
     flex: 1,
@@ -58,6 +115,64 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     resizeMode: 'contain',
+  },
+  modalContainer: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  question: {
+    fontSize: 20,
+    color: color.americanBlue,
+    fontFamily: 'NotoSansBold',
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  choiceButton: {
+    backgroundColor: color.purple,
+    padding: 10,
+    width: 200,
+    borderRadius: 5,
+    marginVertical: 5,
+    alignItems: 'center',
+  },
+  choiceButtonText: {
+    color: 'white',
+    fontFamily: 'NotoSansBold'
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  statusText: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  correctStatus: {
+    color: 'green',
+  },
+  incorrectStatus: {
+    color: 'red',
+  },
+  explanationText: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  explanationDetails: {
+    // Style your long explanation text here
+  },
+  returnButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 15,
+    alignItems: 'center',
+  },
+  returnButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
 
